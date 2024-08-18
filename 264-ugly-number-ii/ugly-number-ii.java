9f8f82b08588e4db55da5f6bcfc4013e05f5044c
Shahil <104596962+shahil1616@users.noreply.github.com>
2024-08-19 00:50:03 +0530

    Time: 51 ms (18.62%) | Memory: 44.6 MB (17.43%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public int nthUglyNumber(int n) {
+        TreeSet<Long> uglyNumbersSet = new TreeSet<>(); // TreeSet to store potential ugly numbers
+        uglyNumbersSet.add(1L); 
+
+        Long currentUgly = 1L;
+        for (int i = 0; i < n; i++) {
+            currentUgly = uglyNumbersSet.pollFirst(); 
+
+         
+            uglyNumbersSet.add(currentUgly * 2);
+            uglyNumbersSet.add(currentUgly * 3);
+            uglyNumbersSet.add(currentUgly * 5);
+        }
+
+        return currentUgly.intValue(); // Return the nth ugly number
+    }
+}
